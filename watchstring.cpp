2013dc70b87bc75c242db523ef88bd9e0cad68e6
Mifia <xzx._@outlook.com>
2023-10-19 13:09:45 +0300

    Update watchstring.cpp

--- watchstring.cpp
+++ watchstring.cpp
@@ -1,21 +1,27 @@
 #include "pin.H"
 #include <iostream>
 #include <cstring>
+#include <fstream> 
 
-// Launch with ./pin -pid 43242 -t watchstring.so -s "UniqueString"
+// Launch it with ./pin -pid 43242 -t watchstring.so -s "UniqueString" -f "/path/to/logfile.txt"
 // https://www.youtube.com/watch?v=A3iRjGdyoLo
 // Download library https://www.intel.com/content/www/us/en/developer/articles/tool/pin-a-dynamic-binary-instrumentation-tool.html
 
+// Global ofstream object for writing to the file
+std::ofstream logFile;
+
 KNOB<string> TargetString(KNOB_MODE_WRITEONCE, "pintool", "s", "", "target string to detect");
+KNOB<string> LogFilePath(KNOB_MODE_WRITEONCE, "pintool", "f", "", "path to the log file");
 
 VOID WriteMem(VOID * ip, VOID * addr, UINT32 size) {
     const char* target = TargetString.Value().c_str();
     size_t target_length = strlen(target);
+    // logFile << "Memory write detected" << std::endl; // Add to debug
     
     // Loop through the memory write to see if it contains the target string
     for (UINT32 i = 0; i <= size - target_length; i++) {
         if (strncmp((char*)addr + i, target, target_length) == 0) {
-            printf("Memory write containing '%s' detected at IP: %p\n", target, ip);
+            logFile << "Memory write containing '" << target << "' detected at IP: " << ip << std::endl;
             break;
         }
     }
@@ -44,10 +50,20 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    // Open the log file
+    logFile.open(LogFilePath.Value().c_str(), std::ios::out | std::ios::app); // Open in append mode
+    if (!logFile.is_open()) {
+        std::cerr << "Failed to open the log file: " << LogFilePath.Value() << std::endl;
+        return -1;
+    }
+
     INS_AddInstrumentFunction(Instruction, 0);
     
     // Start the program
     PIN_StartProgram();
     
+    // Close the log file (though this point might not be reached if the program doesn't naturally terminate)
+    logFile.close();
+
     return 0;
 }
